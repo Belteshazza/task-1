835b188c38c36222953b9023fea387ef6de5f2c9
bethesida <idongesitutong@gmail.com>
2024-07-02 14:04:15 +0100

    completed hng task 1

---
+++ app/Http/Controllers/VisitorController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class VisitorController extends Controller
+{
+    public function visitor(Request $request)
+    {
+        $visitorName = $request->input('visitor_name', 'Anonymous');
+
+        $Ip = $request->ip();
+    
+        $ipResponse = Http::get("http://ip-api.com/json");
+        
+        if ($ipResponse->successful()) {
+            $locData = $ipResponse->json();
+            $loc = $locData['city'] . ', ' . $locData['country'];
+        } else {
+            $loc = 'Unknown';
+            $ipErrorMessage = 'Error fetching location data: ' . $ipResponse->status() . ' - ' . $ipResponse->body();
+            Log::error($ipErrorMessage);
+
+        
+        }
+
+        $weathResponse = Http::get("http://api.weatherapi.com/v1/current.json", [
+            'key' => 'c871a5ce62624d7d800113441240207',
+            'q' => $loc,
+            'aqi' => 'no',
+        ]);
+
+        if ($weathResponse->successful()) {
+            $weathData = $weathResponse->json();
+            $temp = $weathData['current']['temp_c'];
+        } else {
+            $temp = 'N/A';
+            $weathErrorMessage = 'Error fetching weather data: ' . $weathResponse->status() . ' - ' . $weathResponse->body();
+            Log::error($weathErrorMessage);
+        }
+
+        $response = [
+            'client_ip' => $Ip,
+            'location' => $loc,
+            'greeting' => "Hello, $visitorName!, the temperature is $temp degrees Celsius in $loc",
+        ];
+
+       // dd($response);
+        return response()->json($response);
+    }
+}
